21c0244fa6bb1688dfcda40b9770fceeabb95324
Ryan Lambert <ryan@rustprooflabs.com>
2019-06-04 22:07:33 +0000

    Adding passing unit tests.
    Update README
    Add .gitignore
    
    Add error handling to config RE logging path

---
+++ tests/piws_arduino_station_test.py
@@ -0,0 +1,18 @@
+""" Unit tests to cover the Arduino Station module."""
+import unittest
+from piws import arduino_station
+
+TEMP_C = 100
+TEMP_F = 212.0
+
+class ChartsTests(unittest.TestCase):
+
+    def test_convert_c_to_f_returns_WHAT_ARE_WE_TESTING(self):
+        result = arduino_station.convert_c_to_f(TEMP_C)
+        expected = float
+        self.assertEqual(expected, type(result))
+
+    def test_convert_c_to_f_returns_correct_temp_F_value(self):
+        result = arduino_station.convert_c_to_f(TEMP_C)
+        expected = TEMP_F
+        self.assertEqual(expected, result)
